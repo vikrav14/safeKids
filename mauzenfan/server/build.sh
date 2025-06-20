027f119843ba193f1cacbef7ddc99324c846fe48
vikrav14 <vikrav14@gmail.com>
2025-06-20 19:48:46 +0400

    moving django project dir

--- mauzenfan/server/build.sh
+++ mauzenfan/server/build.sh
@@ -2,9 +2,15 @@
 # exit on error
 set -o errexit
 
-# Install dependencies using Poetry - this will handle everything
+# Navigate to project root where pyproject.toml should be
+cd /opt/render/project/src
+
+# Install dependencies using Poetry
 poetry install --no-interaction --no-ansi
 
-# Run management commands using the virtualenv Python
+# Move to your Django project directory
+cd mauzenfan/server
+
+# Run management commands
 /opt/render/project/src/.venv/bin/python manage.py collectstatic --noinput
 /opt/render/project/src/.venv/bin/python manage.py migrate
