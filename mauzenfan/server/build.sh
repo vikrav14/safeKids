ecaf13f142f1e1a307a2a9bcb16688897198f9ea
vikrav14 <vikrav14@gmail.com>
2025-06-20 19:39:44 +0400

    tring to use poetry

--- mauzenfan/server/build.sh
+++ mauzenfan/server/build.sh
@@ -2,9 +2,13 @@
 # exit on error
 set -o errexit
 
-# Install dependencies using the default pip
-pip install -r requirements.txt
+# Install system dependencies
+sudo apt-get update
+sudo apt-get install -y libpq-dev build-essential python3-dev
 
-# Now, use the EXPLICIT path to the python in the virtualenv for all manage.py commands
+# Use Poetry for Python dependency management
+poetry install --no-interaction --no-ansi
+
+# Now, use the explicit path to the python in the virtualenv
 /opt/render/project/src/.venv/bin/python manage.py collectstatic --noinput
 /opt/render/project/src/.venv/bin/python manage.py migrate
