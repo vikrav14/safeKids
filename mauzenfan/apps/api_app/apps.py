9e414cdd045c9f0917a03200bde96127c52a2a50
vikrav14 <vikrav14@gmail.com>
2025-06-21 22:42:10 +0400

    updae

--- mauzenfan/apps/api_app/apps.py
+++ mauzenfan/apps/api_app/apps.py
@@ -2,7 +2,7 @@ from django.apps import AppConfig
 
 class api_appConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'api_app'
+    name = 'apps.api_app'
 
     def ready(self):
         # Add this line back, but corrected
