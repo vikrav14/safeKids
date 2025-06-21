3c9150096180f23ae17a6c58c5e2b9960f9c17cc
vikrav14 <vikrav14@gmail.com>
2025-06-21 21:19:17 +0400

    update maage.py

--- mauzenfan/server/manage.py
+++ mauzenfan/server/manage.py
@@ -2,26 +2,24 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent
-sys.path.append(str(BASE_DIR))
-
-
 def main():
-    """Run administrative tasks."""
+    # Calculate base directory
+    BASE_DIR = Path(__file__).resolve().parent
+    
+    # Add project root to Python path
+    sys.path.append(str(BASE_DIR))
+    
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mauzenfan_config.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            "Couldn't import Django. Are you sure it's installed and available on your PYTHONPATH environment "
+            "variable? Did you forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
