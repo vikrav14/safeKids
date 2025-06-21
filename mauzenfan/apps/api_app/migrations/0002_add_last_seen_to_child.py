d314ad7c93a1d1f081f23a60242c63117a397918
vikrav14 <vikrav14@gmail.com>
2025-06-21 22:00:49 +0400

    proper indent

--- mauzenfan/apps/api_app/migrations/0002_add_last_seen_to_child.py
+++ mauzenfan/apps/api_app/migrations/0002_add_last_seen_to_child.py
@@ -5,14 +5,14 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-dependencies = [
-    ('api_app', '0001_initial'),  # Correct app name
-]
+    dependencies = [
+        ('api_app', '0001_initial'),  # Make sure this is indented properly
+    ]
 
     operations = [
         migrations.AddField(
             model_name='child',
-            name='last_seen_at',
+            name='last_seen',
             field=models.DateTimeField(blank=True, null=True),
         ),
     ]
