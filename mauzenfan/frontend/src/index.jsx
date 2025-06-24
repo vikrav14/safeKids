cd3eb4491474aceb57bad9bd928eb11f068071b9
vikrav14 <vikrav14@gmail.com>
2025-06-24 17:22:52 +0400

    rename

--- mauzenfan/frontend/src/index.jsx
+++ mauzenfan/frontend/src/index.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App.tsx';
+import App from './App.jsx';
 import { AuthProvider } from './contexts/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
